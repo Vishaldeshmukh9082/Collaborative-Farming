11173fab86c2f154a9bc87ae24d651a402db4664
Vishaldeshmukh9082 <deshmukhvishal2002@gmail.com>
2024-04-15 23:17:37 +0530

    second commit

--- app/src/main/java/com/vishal/collaborativefarming/Client_HomePage.java
+++ app/src/main/java/com/vishal/collaborativefarming/Client_HomePage.java
@@ -7,37 +7,59 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
-public class Client_HomePage extends AppCompatActivity {
+public class Client_HomePage extends AppCompatActivity implements OnClickListener {
     CardView addfeild,profile,viewapplication,paperwork,companys;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_home_page);
 
-        addfeild= findViewById(R.id.addfeildcard);
+        addfeild=  findViewById(R.id.addfeildcard);
         profile=findViewById(R.id.profilecard);
+        viewapplication=findViewById(R.id.viewapplicationcard);
+        paperwork=findViewById(R.id.paperworkcard);
+        companys=findViewById(R.id.companyscard);
 
-        addfeild.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Client_HomePage.this,Add_FeildPage.class);
-                startActivity(intent);
-                Toast.makeText(Client_HomePage.this, "add feild clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-//
-        profile.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(Client_HomePage.this,Client_ProfilePage.class);
-                startActivity(intent);
-            }
-        });
+        addfeild.setOnClickListener(this);
+        profile.setOnClickListener(this);
+        viewapplication.setOnClickListener(this);
+        paperwork.setOnClickListener(this);
+        companys.setOnClickListener(this);
+
 
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        switch(view.getId()){
+            case R.id.addfeildcard:
+                i=new Intent(this,Add_FeildPage.class);
+                startActivity(i);
+                break;
+            case R.id.profilecard:
+                i=new Intent(this, Client_ProfilePage.class);
+                startActivity(i);
+                break;
+            case R.id.viewapplicationcard:
+                i=new Intent(this,View_ApplicationPage.class);
+                startActivity(i);
+                break;
+            case R.id.paperworkcard:
+                Toast.makeText(this, "Paper Work Clicked", Toast.LENGTH_SHORT).show();
+//                i=new Intent(this,Add_FeildPage.class);
+
+                break;
+            case R.id.companyscard:
+//                i=new Intent(this,Add_FeildPage.class);
+                Toast.makeText(this, "Companys option Clicked", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/vishal/collaborativefarming/View_ApplicationPage.java
@@ -0,0 +1,14 @@
+package com.vishal.collaborativefarming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_ApplicationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_application_page);
+    }
+}
